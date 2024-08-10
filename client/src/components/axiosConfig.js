b6180e0682c044de1546dfed36d84a7d4e2e5a7f
joao marcelo neres vieira <john.collins.vieira@gmail.com>
2024-08-10 18:17:06 -0400

    frontend deployment

--- client/src/components/axiosConfig.js
+++ client/src/components/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://book-club-q8mm.onrender.com',
+  baseURL: 'https://book-club-api-1hda.onrender.com',
   withCredentials: true, 
 });
 
